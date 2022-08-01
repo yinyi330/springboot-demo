82b740072144b596094a69d301870f45461cb691
yinyi <yy18683783118@163.com>
2022-08-01 22:48:50 +0800

    【代码提交】添加sawwger配置信息

--- src/main/java/com/yinyispringbootdemo/config/WebConfig.java
+++ src/main/java/com/yinyispringbootdemo/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.web.config;
+package  com.yinyispringbootdemo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
