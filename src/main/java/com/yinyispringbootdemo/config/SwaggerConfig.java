d2bfa3a7f16e14414d0e6c759a9bf034031589d8
yinyi <yy18683783118@163.com>
2022-08-03 21:40:47 +0800

    【代码提交】添加sawwger配置信息

--- src/main/java/com/yinyispringbootdemo/config/SwaggerConfig.java
+++ src/main/java/com/yinyispringbootdemo/config/SwaggerConfig.java
@@ -7,29 +7,75 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket createRestApi() {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                //为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("com.web.controller"))
+                //swagger测试的controller路径
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                //下面两行为header中的Authorization认证，如不需要可不添加
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
     }
 
-    //构建 api文档的详细信息函数
+    //在测试界面简单进行描述
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("API接口")
+                .title("APIs")
+                .description("api接口文档")
                 .version("1.0")
                 .build();
     }
+
+    private List<ApiKey> securitySchemes() {
+        //设置请求头信息
+        List<ApiKey> result = new ArrayList<>();
+        ApiKey apiKey = new ApiKey("Authorization", "Authorization", "header");
+        result.add(apiKey);
+        return result;
+    }
+
+    private List<SecurityContext> securityContexts() {
+        //设置需要登录认证的路径
+        List<SecurityContext> result = new ArrayList<>();
+        result.add(getContextByPath("/*/.*"));
+        return result;
+    }
+
+    private SecurityContext getContextByPath(String pathRegex){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(pathRegex))
+                .build();
+    }
+
+    //设置为全局调用，在测试界面输入一个Authorization值，后面测试的接口都将添加Authorization
+    private List<SecurityReference> defaultAuth() {
+        ArrayList<SecurityReference> result = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        result.add(new SecurityReference("Authorization", authorizationScopes));
+        return result;
+    }
 }
+
