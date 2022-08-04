eaebb1b8d81319a00150020aa80c309179ba7123
yinyi <yy18683783118@163.com>
2022-08-04 21:32:54 +0800

    【代码提交】添加德鲁伊和mybatis-plus的简单配置

--- src/main/java/com/yinyispringbootdemo/config/WebConfig.java
+++ src/main/java/com/yinyispringbootdemo/config/WebConfig.java
@@ -1,24 +1,22 @@
 package  com.yinyispringbootdemo.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.
-                addResourceHandler("/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
-                .resourceChain(false);
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/swagger-ui/")
-                .setViewName("forward:/swagger-ui/index.html");
-    }
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/*")
+//                .setViewName("forward:/swagger-ui.html");
+//    }
 }
--- src/main/java/com/yinyispringbootdemo/controller/TestController.java
+++ src/main/java/com/yinyispringbootdemo/controller/TestController.java
@@ -1,25 +1,28 @@
 package com.yinyispringbootdemo.controller;
 
+import com.alibaba.druid.stat.DruidStatManagerFacade;
 import com.yinyispringbootdemo.entity.User;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
     @GetMapping("/testSwagger")
     @ApiOperation(value = "swagger测试")
-    public String swagger(){
+    public String swagger() {
         return "hello, swagger!";
     }
 
     @GetMapping("/testToken")
     @ApiOperation(value = "swagger中token测试")
-    public String token(HttpServletRequest httpServletRequest){
+    public String token(HttpServletRequest httpServletRequest) {
         //返回请求头中Authorization的值
-        if(httpServletRequest.getHeader("Authorization") == null){
+        if (httpServletRequest.getHeader("Authorization") == null) {
             return "Authorization is null";
         }
         return httpServletRequest.getHeader("Authorization");
@@ -27,18 +30,25 @@ public class TestController {
 
     @GetMapping("/testParams")
     @ApiOperation(value = "swagger中token测试")
-    public String testParams(HttpServletRequest httpServletRequest, @RequestParam("name")String name){
+    public String testParams(HttpServletRequest httpServletRequest, @RequestParam("name") String name) {
 
-        return  name+"你好！";
+        return name + "你好！";
     }
 
     @PostMapping("/testJson")
     @ApiOperation(value = "swagger中token测试")
-    public String testJson(HttpServletRequest httpServletRequest, @RequestBody User user){
+    public String testJson(HttpServletRequest httpServletRequest, @RequestBody User user) {
 
-        return  user.toString();
+        return user.toString();
     }
 
+    @GetMapping("/stat")
+    public List druidStat() {
+        // DruidStatManagerFacade#getDataSourceStatDataList 该方法可以获取所有数据源的监控数据
+        List<Map<String, Object>>  list = DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
+        return list;
+
+    }
 
 
 }
